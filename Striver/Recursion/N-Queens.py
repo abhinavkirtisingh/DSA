504bdfe21eb05fbad1896da831544b815e0ce5b1
abhinavkirtisingh <abhinavkirtisingh@gmail.com>
2025-06-12 21:54:14 +0530

    new approach for n queens using hashin

--- Striver/Recursion/N-Queens.py
+++ Striver/Recursion/N-Queens.py
@@ -42,4 +42,42 @@ class Solution:
         return final
 
 
+# Hasing Approach 
+#lower diagonal formula = row + col
+#upper diagonal formula = (n-1) + col + row       
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
         
+
+
+        final = []
+        upper = [0] * ((2*n) -1)
+        lower = [0] * ((2*n) -1)
+        side = [0] * n
+        def solveQ(ans,col,side,lower,upper):
+            
+            if col == n:
+                temp = []
+                for i in range(n):
+                    temp.append(''.join(ans[i]))
+                final.append(temp.copy())
+                return
+            
+            for row in range(n):
+                
+                if side[row] == 0 and lower[row+col] == 0 and upper[(n-1) + col - row] == 0:
+                    ans[row][col] = 'Q'
+                    side[row] = 1
+                    lower[row+col] = 1
+                    upper[(n-1) + col - row] = 1
+                    solveQ(ans,col+1,side,lower,upper)                        
+                    ans[row][col] = '.'
+                    side[row] = 0
+                    lower[row+col] = 0
+                    upper[(n-1) + col - row] = 0
+        ans = [['.' for i in range(n)] for j in range(n)]
+        solveQ(ans,0,side,lower,upper)  
+        return final
