f312f200faf7d46dceaf9ac44f53befc922c26e6
abhinavkirtisingh <abhinavkirtisingh@gmail.com>
2025-07-10 09:11:12 +0530

    LIS with binary search

--- Striver/Dynamic Programming/DP on LIS/Longest Increasing Subsequence.py
+++ Striver/Dynamic Programming/DP on LIS/Longest Increasing Subsequence.py
@@ -1,3 +1,5 @@
+# Memorization
+# https://leetcode.com/problems/longest-increasing-subsequence/
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
 
@@ -18,3 +20,56 @@ class Solution:
             return dp[index][prev]
         
         return fun(0,-1) 
+
+# Tabulation
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        N = len(nums)
+        mx = max(nums)
+        dp = [[0 for i in range(N+1)] for j in range(N+2)]
+        
+        for index in range(N,-1,-1):
+            for prev in range(index-1,-1,-1):
+                temp = 0
+                if prev == 0 or nums[index-1] > nums[prev-1]:
+                    temp = 1 + dp[index+1][index]
+                    
+                dp[index][prev] = max(dp[index+1][prev], temp)
+        # print(dp)
+        return dp[1][0]
+
+
+# Binary search and best complexity approach
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        def lowerB(arr,target):
+            low = 0
+            high = len(arr)-1
+            
+            while low <= high:
+                
+                mid = (low + high)//2
+                
+                if arr[mid] >= target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return low
+        
+        ans = [nums[0]]
+
+        for i in range(1,len(nums)):
+            
+            if nums[i] <= ans[-1]:
+                index = lowerB(ans,nums[i])
+                ans[index] = nums[i]
+            else:
+                ans.append(nums[i])
+        return len(ans)
+
+
+
+        
